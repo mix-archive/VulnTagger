eb550e7977a71d92ace9ab3a8fa7f9f18369ee80
Mix <32300164+mnixry@users.noreply.github.com>
2024-03-04 13:45:40 +0800

    Change ownership and permissions of flag file, update user for running bot.py

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,11 @@ fi
 
 echo -n $FLAG > /flag
 unset FLAG
+chown bot:bot /flag
+chmod 400 /flag
 
 su app -c 'python -m vulntagger' &
-su app -c 'python /app/bot.py'
+su bot -c 'python /app/bot.py'
 
 # Kill all processes
 kill -9 -1
